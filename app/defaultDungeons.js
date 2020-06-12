d17e4c16dc33422753fd513eed78efad893d9f79
thecodeflayer <thecodeflayer@users.noreply.github.com>
2020-06-12 07:49:40 -0600

    more work on dungeons

--- app/defaultDungeons.js
+++ app/defaultDungeons.js
@@ -3,42 +3,42 @@ export const defaultDungeons = {
         boss: false,
         crystal: 0,
         maxCrystal: 4,
-        chests: 7,
-        maxChests: 7,
+        chests: 3,
+        maxChests: 3,
         map: false,
         compass: false,
         bosskey: false,
         smallkeys: 0,
-        maxSmallkeys: 7
+        maxSmallkeys: 2
     }, // eastern palace
     dp: {
         boss: false,
         crystal: 0,
         maxCrystal: 4,
-        chests: 7,
-        maxChests: 7,
+        chests: 2,
+        maxChests: 2,
         map: false,
         compass: false,
         bosskey: false,
         smallkeys: 0,
-        maxSmallkeys: 7
+        maxSmallkeys: 4
     }, // desert palace
     toh: {
         boss: false,
         crystal: 0,
         maxCrystal: 4,
-        chests: 7,
-        maxChests: 7,
+        chests: 2,
+        maxChests: 2,
         map: false,
         compass: false,
         bosskey: false,
         smallkeys: 0,
-        maxSmallkeys: 7
+        maxSmallkeys: 1
     }, // tower of harrah
     aga: {
         boss: false,
         crystal: 0,
-        maxCrystal: 4,
+        maxCrystal: 0,
         chests: 0,
         maxChests: 0,
         map: false,
@@ -51,96 +51,96 @@ export const defaultDungeons = {
         boss: false,
         crystal: 0,
         maxCrystal: 4,
-        chests: 7,
-        maxChests: 7,
+        chests: 5,
+        maxChests: 5,
         map: false,
         compass: false,
         bosskey: false,
         smallkeys: 0,
-        maxSmallkeys: 7
+        maxSmallkeys: 6
     }, // palace of darkness
     sp: {
         boss: false,
         crystal: 0,
         maxCrystal: 4,
-        chests: 7,
-        maxChests: 7,
+        chests: 6,
+        maxChests: 6,
         map: false,
         compass: false,
         bosskey: false,
         smallkeys: 0,
-        maxSmallkeys: 7
+        maxSmallkeys: 5
     }, // swamp palace
     sw: {
         boss: false,
         crystal: 0,
         maxCrystal: 4,
-        chests: 7,
-        maxChests: 7,
+        chests: 2,
+        maxChests: 2,
         map: false,
         compass: false,
         bosskey: false,
         smallkeys: 0,
-        maxSmallkeys: 7
+        maxSmallkeys: 5
     }, // skull woods
     tt: {
         boss: false,
         crystal: 0,
         maxCrystal: 4,
-        chests: 7,
-        maxChests: 7,
+        chests: 4,
+        maxChests: 4,
         map: false,
         compass: false,
         bosskey: false,
         smallkeys: 0,
-        maxSmallkeys: 7
+        maxSmallkeys: 3
     }, // thieves town
     ip: {
         boss: false,
         crystal: 0,
         maxCrystal: 4,
-        chests: 7,
-        maxChests: 7,
+        chests: 3,
+        maxChests: 3,
         map: false,
         compass: false,
         bosskey: false,
         smallkeys: 0,
-        maxSmallkeys: 7
+        maxSmallkeys: 6
     }, // ice palace
     mm: {
         boss: false,
         crystal: 0,
         maxCrystal: 4,
-        chests: 7,
-        maxChests: 7,
+        chests: 2,
+        maxChests: 2,
         map: false,
         compass: false,
         bosskey: false,
         smallkeys: 0,
-        maxSmallkeys: 7
+        maxSmallkeys: 6
     }, // misery mire
     tr: {
         boss: false,
         crystal: 0,
         maxCrystal: 4,
-        chests: 7,
-        maxChests: 7,
+        chests: 5,
+        maxChests: 5,
         map: false,
         compass: false,
         bosskey: false,
         smallkeys: 0,
-        maxSmallkeys: 7
+        maxSmallkeys: 6
     }, // turtle rock
     agatoo: {
         boss: false,
         crystal: 0,
-        maxCrystal: 4,
-        chests: 7,
-        maxChests: 7,
+        maxCrystal: 0,
+        chests: 20,
+        maxChests: 20,
         map: false,
         compass: false,
         bosskey: false,
         smallkeys: 0,
-        maxSmallkeys: 7
+        maxSmallkeys: 8
     } // aganhim2
 }
