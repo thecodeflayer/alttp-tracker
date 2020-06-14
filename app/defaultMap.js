725e13f02a1cef9afbf1dc9546358c2813779788
thecodeflayer <thecodeflayer@users.noreply.github.com>
2020-06-14 09:31:57 -0600

    Bind lightworld map properties to ModelManager.js

---
+++ app/defaultMap.js
@@ -0,0 +1,15 @@
+export const defaultMap = {
+    version: '0.0.1',
+    data: {
+        locations:{},
+        lightworld: {
+            mode:0, //0 for map, 1 for item list
+            scale: 0.5,
+            top: -375,
+            left: -375,
+            x: -375,
+            y: -375
+        },
+        darkworld: {}
+    }
+}
