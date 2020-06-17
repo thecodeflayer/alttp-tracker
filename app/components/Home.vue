06bcaf9beaa8aa19179b1df3a064f192858daef1
thecodeflayer <thecodeflayer@users.noreply.github.com>
2020-06-17 14:40:49 -0600

    Shoutouts and nav fixes.

--- app/components/Home.vue
+++ app/components/Home.vue
@@ -1,30 +1,37 @@
 <template>
-    <Page>
+    <Page backgroundColor="black">
         <Navbar></Navbar>
-        <GridLayout columns="*", rows="50,50,50,50" style="margin-top:10">
-            <Button row="0" col="0" class="btn" @tap="resetItems">Reset Items</Button>
-            <Button row="1" col="0" class="btn" @tap="resetDungeons">Reset Dungeons</Button>
-            <Button row="2" col="0" class="btn" @tap="resetMap">Reset Map</Button>
-        </GridLayout>
+        <ScrollView>
+            <StackLayout orientation="vertical" horizontalAlignment="center" verticalAlignment="top">
+                <Label text="Welcome to the ALLTP Tracker!" class="welcome" fontSize="24" textAlignment="center"/>
+                <Label text="Track item collection and dungeon progress as well as find locations using the maps." class="welcome" textWrap="true" fontSize="20"  textAlignment="center"/>
+                <Label text="More features will be coming as I have time including other modes and glitches." class="welcome" textWrap="true" fontSize="20"  textAlignment="center"/>
+                <Label text="Special thanks to the randomizer community. Visit them!" class="welcome" textWrap="true" fontSize="20"  textAlignment="center"/>
+                <Button class="btn" @tap="clickRandomizer">ALTTP Randomizer</Button>
+                <Button class="btn" @tap="clickReddit">ALTTP on Reddit</Button>
+                <Button class="btn" @tap="clickDiscord">ALTTP Discord</Button>
+            </StackLayout>
+        </ScrollView>
     </Page>
 </template>
 
 <script>
-
+    import {openUrl} from 'tns-core-modules/utils/utils';
     export default {
         computed: {
 
         },
         methods: {
-            resetItems() {
-                this.$modelManager.resetItems();
+            clickRandomizer() {
+                openUrl('https://alttpr.com/');
             },
-            resetDungeons() {
-                this.$modelManager.resetDungeons();
+            clickReddit() {
+                openUrl('https://www.reddit.com/r/alttpr/');
             },
-            resetMap() {
-                this.$modelManager.resetMap();
+            clickDiscord() {
+                openUrl('https://discord.gg/48sauwx');
             }
+
         }
     };
 </script>
@@ -36,7 +43,11 @@
     .fas {
         @include colorize($color: accent);
     }
-
+    .welcome {
+        color:white;
+        font-family: "Return of Ganon", "ReturnofGanon";
+        padding: 10;
+    }
     .btn {
         font-size: 20;
         background-color: darkgreen;
@@ -44,6 +55,7 @@
         color: white;
         horizontal-align: center;
         vertical-align: center;
+        font-family: "Return of Ganon", "ReturnofGanon";
         width: 90%
     }
 </style>
