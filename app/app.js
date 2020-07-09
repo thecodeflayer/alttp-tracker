647c52ad88cf8bf9dfd3bb3f93c3955c57a1780f
thecodeflayer <thecodeflayer@users.noreply.github.com>
2020-07-09 11:20:11 -0600

    disable verbose Vue mode

--- app/app.js
+++ app/app.js
@@ -3,7 +3,7 @@ import Home from './components/Home'
 import Navbar from '~/components/Navbar';
 import {ModelManager} from "~/components/ModelManager";
 import {StaticObjectLoader} from "~/components/StaticObjectLoader";
-Vue.config.silent = false;
+//Vue.config.silent = false;
 Vue.component('Navbar', Navbar);
 Vue.prototype.$modelManager = new ModelManager();
 Vue.prototype.$sol = new StaticObjectLoader();
