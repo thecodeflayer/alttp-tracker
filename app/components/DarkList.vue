a5d370b0a261daccc97b66701546e706f27a38dc
thecodeflayer <thecodeflayer@users.noreply.github.com>
2020-07-15 13:27:47 -0600

    More typescript conversion

--- app/components/DarkList.vue
+++ app/components/DarkList.vue
@@ -1,45 +1,49 @@
 <template>
-    <Page>
-        <Navbar></Navbar>
-        <StackLayout orientation="vertical">
-            <GridLayout columns="28,*,*" rows="20" class="top-header">
-                <Image col="0" row="0" height="16" width="16" src="~/img/map_btn.png" style="padding-left:10" @tap="toggleMode" verticalAlignment="top"/>
-                <Label col="1" row="0" style="padding:0 8 0 0" text="Return to Map" @tap="toggleMode"/>
-                <Label col="2" row="0" style="padding:0 8" horizontalAlignment="right" @tap="setFilterList"
-                       :text="'Filter: '+(filterList === 3 ? 'Unavailable' : filterList === 2 ? 'Available' : filterList === 1 ? 'Checked' : 'All')"/>
-            </GridLayout>
-            <ScrollView  ref="listScrollView" orientation="vertical"
-                         :height="mapHandler.keys.length * 54"
-                         :scrollableHeight="mapHandler.keys.length * 54"
-                         @scroll="onScroll">
-                <StackLayout orientation="vertical" backgroundColor="black">
-                    <GridLayout class="locale-wrapper" v-for="key in mapHandler.keys" v-bind:key="key"
-                                :visibility="getVisible(key)"
-                                columns="*,48" rows="60"
-                                :backgroundColor="mapHandler.locations[key].checked ? 'gray' : mapHandler.locations[key].klass === 'locale-green' ? 'darkgreen':'darkred'">
-                        <StackLayout row="0" col="0" orientation="vertical" style="padding-left:5">
-                            <StackLayout orientation="horizontal">
-                                <Image :src="mapHandler.locations[key].checked ? '~/img/checked.png' : '~/img/unchecked.png'" width="32" height="32" @tap="clickCheck(key)" />
-                                <Label class="title" :text="mapHandler.staticLocations[key].title"/>
-                            </StackLayout>
-                            <StackLayout orientation="horizontal" style="padding-right:4;padding-top:6;">
-                                <Image v-for="img in mapHandler.staticLocations[key].req" v-bind:key="img"
-                                       height="16" :src="'~/img/'+img+'.png'" />
-                            </StackLayout>
-                        </StackLayout>
-                        <StackLayout row="0" col="1" orientation="vertical">
-                            <Image row="0" col="1" src="~/img/dungeons/compass1.png" width="32" height="32" @tap="clickCompass(key)" horizontalAlignment="center" />
-                            <StackLayout orientation="horizontal" style="margin-top:4;" horizontalAlignment="right">
-                                <Image src="~/img/chest.png" height="16" width="16" />
-                                <Label class="title" fontSize="16" :text="'x'+mapHandler.staticLocations[key].itemCount"/>
-                            </StackLayout>
+  <Page>
+    <Navbar></Navbar>
+    <StackLayout orientation="vertical">
+      <GridLayout columns="28,*,*" rows="20" class="top-header">
+        <Image col="0" row="0" height="16" width="16" src="~/img/map_btn.png" style="padding-left:10" @tap="toggleMode" verticalAlignment="top"/>
+        <Label col="1" row="0" style="padding:0 8 0 0" text="Return to Map" @tap="toggleMode"/>
+        <Label col="2" row="0" style="padding:0 8" horizontalAlignment="right" @tap="setFilterList"
+               :text="'Filter: '+(filterList === 3 ? 'Unavailable' : filterList === 2 ? 'Available' : filterList === 1 ? 'Checked' : 'All')"/>
+      </GridLayout>
+      <ScrollView ref="listScrollView" orientation="vertical"
+                  :height="mapHandler.keys.length * 54"
+                  :scrollableHeight="mapHandler.keys.length * 54"
+                  @scroll="onScroll">
+        <StackLayout orientation="vertical" backgroundColor="black">
+          <GridLayout class="locale-wrapper" v-for="key in mapHandler.keys" v-bind:key="key"
+                      :visibility="getVisible(key)"
+                      columns="*,48" rows="60"
+                      :backgroundColor="mapHandler.locations[key].checked ? 'gray' : mapHandler.locations[key].klass === 'locale-green' ? 'darkgreen':'darkred'">
+            <StackLayout row="0" col="0" orientation="vertical" style="padding-left:5">
+              <StackLayout orientation="horizontal">
+                <Image
+                    :src="mapHandler.locations[key].checked ? '~/img/checked.png' : '~/img/unchecked.png'"
+                    width="32" height="32" @tap="clickCheck(key)"/>
+                <Label class="title" :text="mapHandler.staticLocations[key].title"/>
+              </StackLayout>
+              <StackLayout orientation="horizontal" style="padding-right:4;padding-top:6;">
+                <Image v-for="img in mapHandler.staticLocations[key].req" v-bind:key="img"
+                       height="16" :src="'~/img/'+img+'.png'"/>
+              </StackLayout>
+            </StackLayout>
+            <StackLayout row="0" col="1" orientation="vertical">
+              <Image row="0" col="1" src="~/img/dungeons/compass1.png" width="32" height="32"
+                     @tap="clickCompass(key)" horizontalAlignment="center"/>
+              <StackLayout orientation="horizontal" style="margin-top:4;" horizontalAlignment="right">
+                <Image src="~/img/chest.png" height="16" width="16"/>
+                <Label class="title" fontSize="16"
+                       :text="'x'+mapHandler.staticLocations[key].itemCount"/>
+              </StackLayout>
 
-                        </StackLayout>
-                    </GridLayout>
-                </StackLayout>
-            </ScrollView>
+            </StackLayout>
+          </GridLayout>
         </StackLayout>
-    </Page>
+      </ScrollView>
+    </StackLayout>
+  </Page>
 </template>
 
 <script type="ts">
@@ -118,35 +122,35 @@
 </script>
 
 <style scoped lang="scss">
-    @import '~@nativescript/theme/scss/variables/forest';
+  @import '~@nativescript/theme/scss/variables/forest';
 
-    // Custom styles
-    .fas {
-        @include colorize($color: accent);
-    }
-    .rog {
-        @include colorize($color: accent);
-    }
-    .top-header {
-        background-color: black;
-        font-family: "Return of Ganon", "ReturnofGanon";
-        font-size: 18;
-        color: white;
-        padding-top: 6;
-    }
-    .locale-wrapper {
-        background-color: darkgreen;
-        margin:4;
-        border-width: 2;
-        border-color: #003400;
-        color: white;
-        padding: 4;
-        font-family: "Return of Ganon", "ReturnofGanon";
-        font-size: 20
-    }
-    .title {
-        font-family: "Return of Ganon", "ReturnofGanon";
-        font-size: 20;
-        padding:5;
-    }
+  // Custom styles
+  .fas {
+    @include colorize($color: accent);
+  }
+  .rog {
+    @include colorize($color: accent);
+  }
+  .top-header {
+    background-color: black;
+    font-family: "Return of Ganon", "ReturnofGanon",serif;
+    font-size: 18;
+    color: white;
+    padding-top: 6;
+  }
+  .locale-wrapper {
+    background-color: darkgreen;
+    margin:4;
+    border-width: 2;
+    border-color: #003400;
+    color: white;
+    padding: 4;
+    font-family: "Return of Ganon", "ReturnofGanon",serif;
+    font-size: 20
+  }
+  .title {
+    font-family: "Return of Ganon", "ReturnofGanon",serif;
+    font-size: 20;
+    padding:5;
+  }
 </style>
