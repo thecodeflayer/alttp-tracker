38438cae78084fb588bd63a972fd6b977a7e253f
thecodeflayer <thecodeflayer@users.noreply.github.com>
2020-07-17 05:20:39 -0600

    Convert more stuff to classes

--- app/inverted/InvertedRegionHelper.ts
+++ app/inverted/InvertedRegionHelper.ts
@@ -1,54 +1,54 @@
-export const invertedRegionHelper = {
+export class InvertedRegionHelper {
   // light world
-  northEastLW(items, dungeons) {
+  static northEastLW(items, dungeons) {
     return dungeons.aga.boss
             || (items.hammer && items.glove > 0 || items.glove === 2);
-  },
-  northWestLW(items, dungeons) {return this.northEastLW(items, dungeons);},
-  southLW(items, dungeons) {return this.northEastLW(items, dungeons);},
-  deathMtnEastLW(items, dungeons) {
+  };
+  static northWestLW(items, dungeons) {return this.northEastLW(items, dungeons); }
+  static southLW(items, dungeons) {return this.northEastLW(items, dungeons); }
+  static deathMtnEastLW(items, dungeons) {
     return items.glove === 2 && this.deathMtnWestDW(items, dungeons) && items.moonpearl;
-  },
-  deathMtnWestLW(items, dungeons) {
+  };
+  static deathMtnWestLW(items, dungeons) {
     return items.flute || (items.glove > 0 && items.lantern);
-  },
+  }
   // dark world
-  mireDW(items, dungeons) {
+  static mireDW(items, dungeons) {
     return items.flute || (items.mirror && this.southLW(items, dungeons));
-  },
-  northEastDW(items, dungeons) {
+  }
+  static northEastDW(items, dungeons) {
     return items.flute || items.hammer || items.flippers || (items.mirror && this.northEastLW(items, dungeons));
-  },
-  northWestDW(items, dungeons) {
+  }
+  static northWestDW(items, dungeons) {
     return true;
-  },
-  southDW(items, dungeons) {
+  }
+  static southDW(items, dungeons) {
     return true;
-  },
-  deathMtnEastDW(items, dungeons) {
+  }
+  static deathMtnEastDW(items, dungeons) {
     return this.deathMtnWestDW(items, dungeons)
             || (items.mirror && items.moonpearl && items.hookshot && this.deathMtnEastLW(items, dungeons));
-  },
-  deathMtnWestDW(items, dungeons) {
+  }
+  static deathMtnWestDW(items, dungeons) {
     return items.flute || (items.glove > 0 && items.lantern);
-  },
-  toh(items, dungeons) {
+  }
+  static toh(items, dungeons) {
     return (items.mirror || (items.hookshot && items.hammer))
             && this.deathMtnWestLW(items, dungeons);
-  },
-  mm(items, dungeons) {
+  }
+  static mm(items, dungeons) {
     const m = (dungeons.mm.medallion === 1 && items.bombos) ||
             (dungeons.mm.medallion === 2 && items.ether) ||
             (dungeons.mm.medallion === 3 && items.quake);
     return m && items.moonpearl && this.mireDW(items, dungeons);
-  },
-  tr(items, dungeons) {
+  }
+  static tr(items, dungeons) {
     const m = (dungeons.tr.medallion === 1 && items.bombos) ||
             (dungeons.tr.medallion === 2 && items.ether) ||
             (dungeons.tr.medallion === 3 && items.quake);
     return m && items.moonpearl && items.redcane && items.hammer && items.glove === 2 && this.deathMtnEastLW(items, dungeons);
-  },
-  gt(items, dungeons) {
+  }
+  static gt(items, dungeons) {
     let c = 0;
     const keys = Object.keys(dungeons);
     for(const key of keys) {
@@ -58,4 +58,4 @@ export const invertedRegionHelper = {
     }
     return c === 7 && items.moonpearl && this.northEastLW(items, dungeons);
   }
-};
+}
