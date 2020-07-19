da3c28d7c2cbcf670b13934e52a1c92c8704c155
thecodeflayer <thecodeflayer@users.noreply.github.com>
2020-07-19 12:42:58 -0600

    add indicators for goal modes to items page.

--- app/components/Items.vue
+++ app/components/Items.vue
@@ -6,13 +6,31 @@
         <GridLayout class="item-grid" columns="*,*,*,*,*,*" rows="40,40,40,40,40">
           <Image v-for="(key, index) in itemKeys" v-bind:key="key" :row="Math.floor(index/6)" :col="index % 6" :src="items[key]" class="item" @tap="clickItem(key)"/>
         </GridLayout>
-        <GridLayout class="item-grid" columns="*,*,*,*,*,*" rows="80">
-          <StackLayout orientation="vertical" row="0" col="0" colSpan="3" height="80" paddingTop="4" paddingLeft="10">
+        <GridLayout class="item-grid" columns="*,*,*,*,*,*" rows="100">
+          <StackLayout orientation="vertical" row="0" col="0" colSpan="3" height="100" paddingTop="4" paddingLeft="10">
             <StackLayout :visibility="goal === 'triforce' ? 'visible':'collapsed'" orientation="horizontal" class="item-count" height="32"
                          horizontalAlignment="left" @tap="clickTriforce">
               <Image src="~/img/items/triforce.png" width="20" height="20" verticalAlignment="center" />
               <Label :text="'('+triforceCount+'/'+triforceGoal+')'" verticalAlignment="center" style="padding-left:5"/>
             </StackLayout>
+            <StackLayout :visibility="goal === 'pedestal' ? 'visible':'collapsed'" orientation="horizontal" class="item-count" height="32"
+                         horizontalAlignment="left">
+              <Image :src="pedestalPendants.g ? '~/img/crystal-green.png':'~/img/crystal-disabled.png'" width="30" height="30" class="item" verticalAlignment="center"/>
+              <Image :src="pedestalPendants.rb0 ? '~/img/dungeons/crystal2.png':'~/img/crystal-disabled.png'" width="30" height="30" class="item" verticalAlignment="center"/>
+              <Image :src="pedestalPendants.rb1 ? '~/img/dungeons/crystal2.png':'~/img/crystal-disabled.png'" width="30" height="30" class="item" verticalAlignment="center"/>
+            </StackLayout>
+            <StackLayout :visibility="goal === 'ganon' || goal === 'fastGanon' ? 'visible':'collapsed'"
+                         orientation="horizontal" class="item-count" height="32" horizontalAlignment="left">
+              <Image v-for="idx in (goal === 'ganon' && openGT > openGanon) ? openGT : openGanon" v-bind:key="idx"
+                :src="crystalCount >= idx ? '~/img/dungeons/crystal3.png' : '~/img/crystal-disabled2.png'"
+                width="16" height="16" marginLeft="3" verticalAlignment="center"/>
+            </StackLayout>
+            <GridLayout :visibility="goal === 'allDungeons' ? 'visible':'collapsed'" orientation="horizontal" class="item-count"
+                        rows="24,24" columns="24,24,24,24,24,24" horizontalAlignment="left">
+              <Image v-for="(boss, idx) in dungeonKeys" v-bind:key="boss" width="20" height="20"
+                     :src="'~/img/dungeons/'+boss+'_boss'+(dungeons[boss].boss ? '1':'0')+'.png'"
+                     :row="Math.floor(idx/6)" :col="idx%6"/>
+            </GridLayout>
             <StackLayout orientation="horizontal" class="item-count" height="32" horizontalAlignment="left">
               <Image src="~/img/chest.png" width="20" height="20" verticalAlignment="center"/>
               <Label :text="'('+itemCount+'/'+itemMax+')'" verticalAlignment="center" style="padding-left:5"/>
@@ -46,9 +64,15 @@
     openGanon = this.$modelManager.settings.openGanon;
     triforceGoal = this.$modelManager.settings.triforceGoal;
     triforceCount = this.$modelManager.settings.triforceCount;
+    pedestalPendants = {rb0:false, rb1:false, g:false};
+    crystalCount = 0;
+    dungeons = this.$modelManager.dungeons;
+    dungeonKeys = Object.keys(this.$modelManager.dungeons);
 
     mounted() {
         this.itemCount = this.$modelManager.getItemCount();
+        this.pedestalPendants = this.getPedestalPendants();
+        this.crystalCount = this.getCrystalCount();
     }
     getImage(key) {
       if (typeof this.$modelManager.getItem(key) === 'boolean') {
@@ -89,6 +113,36 @@
       const keys = this.$modelManager.getItemKeys();
       for(const key of keys) {
         retval[key] = this.getImage(key);
+      }
+      return retval;
+    }
+    getPedestalPendants() {
+      const retval = {rb0:false, rb1:false, g:false};
+      if(this.goal === 'pedestal') {
+        const keys = Object.keys(this.$modelManager.dungeons);
+        let rb = 0;
+        let g = false;
+        for(const key of keys) {
+          if(this.$modelManager.dungeons[key].crystal === 2){
+            rb = rb + 1;
+          }
+          if(this.$modelManager.dungeons[key].crystal === 1) {
+            g = true;
+          }
+        }
+        retval.rb0 = rb > 0;
+        retval.rb1 = rb > 1;
+        retval.g = g;
+      }
+      return retval;
+    }
+    getCrystalCount() {
+      let retval = 0;
+      const keys = Object.keys(this.$modelManager.dungeons);
+      for(const key of keys) {
+        if(this.$modelManager.dungeons[key].crystal > 2){
+          retval = retval +1;
+        }
       }
       return retval;
     }
